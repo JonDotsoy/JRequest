925441095757bfa98789040bfd4e4289dc0dafde
Jonathan Delgado Zamorano <jonad.correo@gmail.com>
2014-06-02 19:51:01 -0400

    Corrige ortografía.

--- request.php
+++ request.php
@@ -1,38 +1,38 @@
 <?php
 
-// config
-$path_module      = "modules/";// almacena la ruta de los modulos
-$path_module_ejec = "main.php";// archivo que es ejecutado por modulo
-$mode_no_ajax     = false; // si es true permite simpre la ajecucion ajax aun que no se alla solicitado
-$out_ajax         = array();// almacena la salida ajax
-$out_html         = ""; // almacena la salida html
-$end_ejec         = array();// almacena los modulos que se deben ejecutar al final
-$err_no_module    = true;// si es true muestra error al no encontrase ejecutro del modulo
-$modules_cache    = array();// almacena todos los modulos ejecutados
+// Configuración app
+$path_module      = "modules/";// Almacena la ruta de los módulos
+$path_module_ejec = "main.php";// Archivo que es ejecutado por modulo
+$mode_no_ajax     = false; // Si es true permite siempre la ejecución Ajax aunque no se allá solicitado
+$out_ajax         = array();// Almacena la salida Ajax
+$out_html         = ""; // Almacena la salida HTML
+$end_ejec         = array();// Almacena los módulos que se deben ejecutar al final
+$err_no_module    = true;// Si es true muestra error al no encontrase ejecutor del modulo
+$modules_cache    = array();// Almacena todos los módulos ejecutados
 
-// comprueba si existe directorio, si no existe lo crea.
+// Comprueba si existe directorio, si no existe lo crea.
 if (!file_exists($path_module)) if (!mkdir($path_module)) throw new Exception("Could not create directory for modules", 1);
 
-// ejecuta todo el listado de modulos y los ejecuta
+// Ejecuta todo el listado de módulos y los ejecuta
 $dir = opendir($path_module);
 while ($file = readdir($dir)) {
-	$mode = "middle";// almacena el periodo en el cual se esta ejecutando el modulo
-	// Exclulle los directorios padres
+	$mode = "middle";// Almacena el periodo en el cual se está ejecutando el modulo
+	// Excluye los directorios padres
 	if ($file != ".." && $file != ".") {
 		if (is_dir($path_module.$file)) {
-			$path_file_coplet = $path_module.$file."/".$path_module_ejec; // contiene la ruta completa del modulo
-			// si el modulo existe lo ejecuta, si no 
+			$path_file_coplet = $path_module.$file."/".$path_module_ejec; // Contiene la ruta completa del modulo
+			// Si el modulo existe lo ejecuta
 			if (file_exists($path_file_coplet)) {
-				$config = array("path"=>$path_file_coplet);// permite llevar al modulo unaconfiguracion que luego le cervira
+				$config = array("path"=>$path_file_coplet);// Permite llevar al módulo una configuración que luego le servirá
 				// Ejecuta el modulo
 				include $path_file_coplet;
-				// detectacta configuracion end_ejec si este es true almacena el modulo y lo ejecuta al final
+				// Detecta configuración 'end_ejec' si este es true almacena el modulo y lo ejecuta al final
 				if (isset($config["end_ejec"]) && $config["end_ejec"] == true) {
 					$end_ejec[] = $config;
 				}
 				$modules_cache[] = $config;
 			} else {
-				// define si se genera o no un error de estar mal contruido el modulo
+				// Define si se genera o no un error de estar mal construido el modulo
 				if ($err_no_module)
 					throw new Exception("The '$file' module has no executor '$path_module_ejec'", 1);
 			}
@@ -40,13 +40,14 @@ while ($file = readdir($dir)) {
 	}
 }
 
-// Ejecuta los modulos al final
+// Ejecuta los módulos declarados como 'end_ejec'
 foreach ($end_ejec as $key => $value) {
-	$mode   = "end";
+	$mode   = "end";// Almacena el periodo en el cual se está ejecutando el modulo
 	$config = $value;
 	include $value["path"];
 }
 
+// Si es llamado con la sentencia 'include' este no se ejecuta.
 if ((count(get_included_files()) <= count($modules_cache)+1))
 	if(
 		(
